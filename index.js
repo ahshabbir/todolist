92ca41d7f27b4e23c0e892f6dfd8697e4e4072a6
Ahad Shabbir <shabbirahadh@gmail.com>
2020-01-07 12:24:20 -0600

    added weekday subdivisions of tasks

--- index.js
+++ index.js
@@ -1,63 +1,100 @@
-// For document listeners:
 window.onload = function() {
-	var taskBox = document.getElementById("new-task-box");
+	var days = getDays();
+
+	// window.curDay represents the state of the application. At start this is the
+	// first item in the left menu i.e. Monday.
+	window.curDay = days[0];
+	window.curDay.weekDayMenuItem.setAttribute("class", "li-active");
+	window.curDay.listContainer.style.display = "initial";
+
+	addHandlers(days);
+	addFormHandlers(
+		document.getElementById("new-task-box"),
+		document.getElementById("add-btn"),
+		document.getElementById("clr-btn")
+	);
+};
+
+function getDays() {
+	var dayList = document.getElementById("day-list").children; // left menu
 	
-	// Add task and clear textbox on enter press:
-	taskBox.addEventListener('keypress', function(e) {
-		if (e.keyCode == 13) {
-			addTask();
-			this.value = "";
+	// All the divs containing both the unfinished and finished lists/
+	var listContainers = document.getElementById("col-2").children;
+	
+	var days = [];
+	for (var i = 0; i < dayList.length; i++) {
+		days.push({
+			weekDayMenuItem: dayList[i],
+			listContainer: listContainers[i]
+		});
+	}
+	return days;
+}
+
+function addHandlers(days) {
+	for (let day of days) {
+		var handler = function(e) {
+			window.curDay.weekDayMenuItem.setAttribute("class", "");
+			window.curDay.listContainer.style.display = "none";
+
+			this.setAttribute("class", "li-active");
+			day.listContainer.style.display = "initial";
+			window.curDay = day;
 		}
+		day.weekDayMenuItem.addEventListener("click", handler, false);
+
+		let unfin = day.listContainer.children[0];
+		let fin = day.listContainer.children[1];
+		day.markDoneHandler = function(e) {
+			unfin.removeChild(this);
+			fin.appendChild(this);
+			this.removeEventListener("click", day.markDoneHandler);
+			this.addEventListener("click", day.markUnDoneHandler);
+		};
+		day.markUnDoneHandler = function(e) {
+			fin.removeChild(this);
+			unfin.appendChild(this);
+			this.removeEventListener("click", day.markUnDoneHandler);
+			this.addEventListener("click", day.markDoneHandler);
+		};
+		for (var i = 0; i < unfin.childElementCount; i++) {
+			unfin.children[i].addEventListener("click", day.markDoneHandler, false);
+		}
+		for (var i = 0; i < fin.childElementCount; i++) {
+			fin.children[i].addEventListener("click", day.markUnDoneHandler, false);
+		}
+	}
+}
+
+function addFormHandlers(taskBox, addBtn, clrBtn) {
+	var addTaskHandler = function (e) {
+		var taskName = taskBox.value;
+		if (taskName === "") {
+			alert("Please enter a task name!");
+			return;
+		}
+
+		var newTask = document.createElement("li");
+		newTask.innerText = taskName;
+		newTask.addEventListener("click", window.curDay.markDoneHandler, false);
+		window.curDay.listContainer.children[0].appendChild(newTask);
+		taskBox.value = "";
+	};
+
+	// Add task and clear textbox on enter press:
+	taskBox.addEventListener("keypress", function(e) {
+		if (e.keyCode == 13)
+			addTaskHandler(e);
+	}, false);
+	
+	addBtn.addEventListener("click", function(e) {
+		addTaskHandler(e);
 	}, false);
 
-	// Clear textbox on enter:
-	taskBox.addEventListener('click', e => { this.value = ""; }, false);
+	clrBtn.addEventListener("click", function(e){
+		window.curDay.listContainer.children[1].innerHTML = "";
+	})
 
-	// Add the MarkDoneListener to existing tasks:
-	var todolist = document.getElementById("unfinished-list");
-	for (var i = 0; i < todolist.childElementCount; i++) {
-		todolist.children[i].addEventListener('click', markDoneHandler, false);
-	}
-
-	// Add the MarkUnDoneListener to existing finished tasks:
-	var finishedList = document.getElementById("finished-list");
-	for (var i = 0; i < finishedList.childElementCount; i++) {
-		finishedList.children[i].addEventListener('click', markUnDoneHandler, false);
-	}
-}
-
-function markDoneHandler(e) {
-	document.getElementById('unfinished-list').removeChild(this);
-	document.getElementById('finished-list').appendChild(this);
-	
-	// Swap the listeners to enable undoing the task:
-	this.removeEventListener('click', markDoneHandler, false);
-	this.addEventListener('click', markUnDoneHandler, false);
-}
-
-function markUnDoneHandler(e) {
-	document.getElementById('finished-list').removeChild(this);
-	document.getElementById('unfinished-list').appendChild(this);
-
-	// Swap the listeners to enable doing the task:
-	this.removeEventListener('click', markUnDoneHandler, false);
-	this.addEventListener('click', markDoneHandler, false);
-}
-
-function addTask() {
-	var taskName = document.getElementById("new-task-box").value;
-	if (taskName === "") {
-		alert("Please enter a task name!");
-		return;
-	}
-	var todolist = document.getElementById("unfinished-list");
-
-	var newTask = document.createElement("li");
-	newTask.innerText = taskName;
-	newTask.addEventListener('click', markDoneHandler, false);
-	todolist.appendChild(newTask);
-}
-
-function clearFinished() {
-	document.getElementById("finished-list").innerHTML = "";
+	// Clear textbox on click:
+	taskBox.addEventListener("click", function(e) { this.value = ""; }, false);
 }
