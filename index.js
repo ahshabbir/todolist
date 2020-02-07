8dfa66b174a752e545eb6c75d73d14940e2cab96
Ahad Shabbir <shabbirahadh@gmail.com>
2020-02-07 00:05:08 -0600

    removed usage of globals and added commets

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-window.onload = function() {
-	var days = getDays();
+window.onload = function () {
+	// We're using an immediately invoked function to take advantage of 
+	// closure for the 'curDay' variable. When this function finishes, curDay
+	// will still exist in memory because of closure, allowing the handlers
+	// to access it and modify the tasks on the correct day.
+	(function() {
+		var days = getDays();
 
-	// window.curDay represents the state of the application. At start this is the
-	// first item in the left menu i.e. Monday.
-	window.curDay = days[0];
-	window.curDay.weekDayMenuItem.setAttribute("class", "li-active");
-	window.curDay.listContainer.style.display = "initial";
+		// curDay represents the state of the application. At start this is the
+		// first item in the left menu (Monday).
+		var curDay = days[0];
+		curDay.weekDayMenuItem.setAttribute("class", "li-active");
+		curDay.listContainer.style.display = "initial";
 
-	addHandlers(days);
-	addFormHandlers(
-		document.getElementById("new-task-box"),
-		document.getElementById("add-btn"),
-		document.getElementById("clr-btn")
-	);
+		addHandlers(days);
+		addFormHandlers(
+			document.getElementById("new-task-box"),
+			document.getElementById("add-btn"),
+			document.getElementById("clr-btn")
+		);
+	})();
 };
 
 function getDays() {
 	var dayList = document.getElementById("day-list").children; // left menu
-	
-	// All the divs containing both the unfinished and finished lists/
+
+	// All the divs containing both the unfinished and finished lists
 	var listContainers = document.getElementById("col-2").children;
-	
+
 	var days = [];
 	for (var i = 0; i < dayList.length; i++) {
 		days.push({
@@ -32,26 +38,29 @@ function getDays() {
 }
 
 function addHandlers(days) {
+	// We're using let here because each day will have it's own space in memory,
+	// ensuring that the handlers modify the correct day.
 	for (let day of days) {
-		var handler = function(e) {
-			window.curDay.weekDayMenuItem.setAttribute("class", "");
-			window.curDay.listContainer.style.display = "none";
+		var handler = function (e) {
+			curDay.weekDayMenuItem.setAttribute("class", "");
+			curDay.listContainer.style.display = "none";
 
 			this.setAttribute("class", "li-active");
 			day.listContainer.style.display = "initial";
-			window.curDay = day;
+			curDay = day;
 		}
 		day.weekDayMenuItem.addEventListener("click", handler, false);
 
+		// let is used for the same reason here
 		let unfin = day.listContainer.children[0];
 		let fin = day.listContainer.children[1];
-		day.markDoneHandler = function(e) {
+		day.markDoneHandler = function (e) {
 			unfin.removeChild(this);
 			fin.appendChild(this);
 			this.removeEventListener("click", day.markDoneHandler);
 			this.addEventListener("click", day.markUnDoneHandler);
 		};
-		day.markUnDoneHandler = function(e) {
+		day.markUnDoneHandler = function (e) {
 			fin.removeChild(this);
 			unfin.appendChild(this);
 			this.removeEventListener("click", day.markUnDoneHandler);
@@ -76,25 +85,25 @@ function addFormHandlers(taskBox, addBtn, clrBtn) {
 
 		var newTask = document.createElement("li");
 		newTask.innerText = taskName;
-		newTask.addEventListener("click", window.curDay.markDoneHandler, false);
-		window.curDay.listContainer.children[0].appendChild(newTask);
+		newTask.addEventListener("click", curDay.markDoneHandler, false);
+		curDay.listContainer.children[0].appendChild(newTask);
 		taskBox.value = "";
 	};
 
 	// Add task and clear textbox on enter press:
-	taskBox.addEventListener("keypress", function(e) {
+	taskBox.addEventListener("keypress", function (e) {
 		if (e.keyCode == 13)
 			addTaskHandler(e);
 	}, false);
-	
-	addBtn.addEventListener("click", function(e) {
+
+	addBtn.addEventListener("click", function (e) {
 		addTaskHandler(e);
 	}, false);
 
-	clrBtn.addEventListener("click", function(e){
-		window.curDay.listContainer.children[1].innerHTML = "";
+	clrBtn.addEventListener("click", function (e) {
+		curDay.listContainer.children[1].innerHTML = "";
 	})
 
 	// Clear textbox on click:
-	taskBox.addEventListener("click", function(e) { this.value = ""; }, false);
+	taskBox.addEventListener("click", function (e) { this.value = ""; }, false);
 }
